6259c0c165fb7eac517c95f5137fce2f6adfc6f3
premprakashrohan <premprakashrohan@gmail.com>
2020-08-21 03:47:54 +0530

    first commit

---
+++ src/test/java/com/ps/quarkus/sample/NativeHelloResourceIT.java
@@ -0,0 +1,9 @@
+package com.ps.quarkus.sample;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeHelloResourceIT extends HelloResourceTest {
+
+    // Execute the same tests but in native mode.
+}
